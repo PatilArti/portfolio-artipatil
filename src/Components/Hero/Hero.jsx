68bdf49de159e020dba2e9c8e4288a773f20c714
PatilArti <artipatilcse2020@gmail.com>
2024-11-05 20:03:59 +0530

    Hero home page code

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import './Hero.css'
-
+import React from "react";
+import "./Hero.css";
+import arti_image from "../../assets/arti_image.jpeg";
 const Hero = () => {
   return (
-    <div className="hero">Hero</div>
-  )
-}
+    <div className="hero">
+      <img className="arti-image" src={arti_image} alt="profile Image" />
+      <h1>
+        <span>I am Arti Patil,</span> frontend developer in India.
+      </h1>
+      <p>
+        Having 4+ Years of Experience in Frontend Development using JavaScript,
+        ReactJS, Redux and web designing including HTML, CSS, Bootstrap and
+        Material UI.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me.</div>
+        <div className="hero-resume">My Resume.</div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
